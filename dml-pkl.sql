0d9a46fcf904f2d9248f2c74ffdbc493f6b445be
prabhatkumar9 <61245217+prabhatkumar9@users.noreply.github.com>
2020-04-08 13:33:35 +0100

    1ST COMMIT

--- dml-pkl.sql
+++ dml-pkl.sql
@@ -2,41 +2,83 @@
 -- PROGRESSION 1:
 
 --1. Insert into city
+--insert into city values(1,'delhi');
+--select * from city;
 
 --2. Insert into referee
+--insert into referee values(1,'refereeName1');
+--select * from referee;
 
 --3. Insert into innings
+--insert into innings values(2,2);
+--select * from innings;
 
 --4. Insert into extra_type
+--insert into extra_type values(1,'extra type1');
 
 --5. Insert into skill
+--insert into skill values(1,'BallCatching');
+--select * from skill;
 
 --6. Insert into team
+--insert into team values(1,'FANBASE','SINHA',1,1);
+--select * from team;
 
 --7. Insert into player
+--INSERT INTO PLAYER VALUES(1,'KL RAHUL','INDIA',1,1);
+-- INSERT INTO PLAYER VALUES(2,'KL RAHUL','INDIA',2,1);
+-- INSERT INTO PLAYER VALUES(3,'MANISH PANDEY','INDIA',1,2);
+-- INSERT INTO PLAYER VALUES(4,'PANDYA','INDIA',1,2);
+--SELECT * FROM PLAYER;
 
 --8. Insert into venue
+--insert into venue values(1,'stadium1',1);
 
 --9. Insert into event
+--insert into event values(1,1,1,1,1010,150,680,1010,1010);
+--select * from EVENT;     integrity constraint
 
 --10. Insert into extra_event
+--insert into extra_event values(1,'EXTRAEVENT1');
+--select * from EXTRA_EVENT;
 
 --11. Insert into outcome
+--insert into outcome values(1,'staTS',1,1,100);
+--integrity constraint
 
 --12. Insert into game
+--insert into game values(1,'21-JAN-23',1,1,1,1,1,1,1,1);
+--integrity constraint 
 
 --13. Update player table
+--SELECT * FROM PLAYER;
+--UPDATE player SET name = 'Lionel Messi' where id=1;
 
 --14. Update player table
+--SELECT * FROM PLAYER;
+--UPDATE player SET name = 'Cristiano Ronaldo' where id=2;
 
 --15. Update player table
+--SELECT * FROM PLAYER;
+--UPDATE player SET name = 'Xavi' where id=3;
 
 --16. Update player table
+--SELECT * FROM PLAYER;
+--UPDATE player SET name = 'Andres Iniesta' where id=4;
 
 --17. Delete from extra_type
+--SELECT * FROM EXTRA_TYPE;
+--DELETE  FROM EXTRA_TYPE WHERE ID=1;
 
 --18. Delete from referee
+--SELECT * FROM REFEREE;
+--INSERT INTO REFEREE VALUES(1,1);
+--DELETE FROM REFEREE WHERE ID=1;
 
 --19. Delete from player
+-- delete from PLAYER where ID=3;
 
 --20. Delete from outcome
+--SELECT * FROM OUTCOME;
+--INSERT INTO OUTCOME VALUES(1,1,1,300,'SACHIN');
+--delete from outcome where score=300;
